e70ec6c875b7f4c27eecff8d4f801c110d3dfbd8
Junye Huang <h.jun.ye@gmail.com>
2022-11-28 16:07:47 +0100

    add utility files and assets

---
+++ assets/parameters.py
@@ -0,0 +1,14 @@
+# game window dimensions
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 750
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT
+SCREEN_HEIGHT = round(WINDOW_HEIGHT * 0.7) # height of pong play field
+
+WIDTH_UNIT = round(WINDOW_WIDTH / 100) # width unit used for scaling the game
+
+# number of the qubits for the quantum circuit
+NUM_QUBITS=3
+# cool down time (in milliseconds) before the next measurement is allowed
+MEASUREMENT_COOLDOWN_TIME = 4000
+# score to win a game
+WIN_SCORE = 1
