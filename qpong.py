e8c5a2b2374d29c9b6be8b03244818694cd422f1
Junye Huang <h.jun.ye@gmail.com>
2022-11-28 16:36:33 +0100

    handle inputs

--- qpong.py
+++ qpong.py
@@ -1,6 +1,7 @@
 import pygame
+import numpy as np
 
-from assets.circuit_grid import CircuitGrid
+from assets.circuit_grid import CircuitGrid, MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT
 
 pygame.init()
 screen = pygame.display.set_mode((1200, 750))
@@ -14,12 +15,46 @@ def main():
 
     exit = False
     while not exit:
+        # update game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
-
-        # update game
-
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    exit = True
+                elif event.key == pygame.K_a:
+                    circuit_grid.move_to_adjacent_node(MOVE_LEFT)
+                elif event.key == pygame.K_d:
+                    circuit_grid.move_to_adjacent_node(MOVE_RIGHT)
+                elif event.key == pygame.K_w:
+                    circuit_grid.move_to_adjacent_node(MOVE_UP)
+                elif event.key == pygame.K_s:
+                    circuit_grid.move_to_adjacent_node(MOVE_DOWN)
+                elif event.key == pygame.K_x:
+                    circuit_grid.handle_input_x()
+                elif event.key == pygame.K_y:
+                    circuit_grid.handle_input_y()
+                elif event.key == pygame.K_z:
+                    circuit_grid.handle_input_z()
+                elif event.key == pygame.K_h:
+                    circuit_grid.handle_input_h()
+                elif event.key == pygame.K_SPACE:
+                    circuit_grid.handle_input_delete()
+                elif event.key == pygame.K_c:
+                    # Add or remove a control
+                    circuit_grid.handle_input_ctrl()
+                elif event.key == pygame.K_UP:
+                    # Move a control qubit up
+                    circuit_grid.handle_input_move_ctrl(MOVE_UP)
+                elif event.key == pygame.K_DOWN:
+                    # Move a control qubit down
+                    circuit_grid.handle_input_move_ctrl(MOVE_DOWN)
+                elif event.key == pygame.K_LEFT:
+                    # Rotate a gate
+                    circuit_grid.handle_input_rotate(-np.pi / 8)
+                elif event.key == pygame.K_RIGHT:
+                    # Rotate a gate
+                    circuit_grid.handle_input_rotate(np.pi / 8)
 
         # draw game
         circuit_grid.draw(screen)
