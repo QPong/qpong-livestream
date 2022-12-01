c5036163f17ea231779d31d7f600a06e4a46605a
Junye Huang <h.jun.ye@gmail.com>
2022-12-01 15:46:13 +0100

    step 3: add classical paddle

--- qpong.py
+++ qpong.py
@@ -1,5 +1,8 @@
 import pygame
 
+from assets import paddle
+from assets.paddle import Paddle
+
 pygame.init()
 screen = pygame.display.set_mode((1200, 750))
 pygame.display.set_caption('QPong')
@@ -7,13 +10,21 @@ clock = pygame.time.Clock()
 
 def main():
 
+    # initialize game
     exit = False
+    classical_paddle = Paddle()
+    moving_sprites = pygame.sprite.Group()
+    moving_sprites.add(classical_paddle)
 
     while not exit:
-        
+        # update game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
+
+        # draw game
+        moving_sprites.draw(screen)
+        pygame.display.flip()
 
         # control framerate
         clock.tick(60)
