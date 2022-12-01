983a0a2d81645d3a3b27998cf5f76092832d8344
Junye Huang <h.jun.ye@gmail.com>
2022-12-01 16:06:00 +0100

    step 5: add classical computer

---
+++ assets/computer.py
@@ -0,0 +1,22 @@
+
+class Computer:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+
+class ClassicalComputer(Computer):
+    def __init__(self, paddle):
+        super().__init__()
+
+        self.paddle = paddle
+        self.score = 0
+        self.speed = 3
+    
+    def update(self, ball):
+        if self.paddle.rect.centery - ball.rect.centery > 0:
+            self.paddle.rect.y -= self.speed
+        else:
+            self.paddle.rect.y += self.speed
+
+
--- qpong.py
+++ qpong.py
@@ -1,6 +1,6 @@
 import pygame
 
-from assets import paddle, ball, globals
+from assets import paddle, ball, globals, computer
 
 pygame.init()
 screen = pygame.display.set_mode((1200, 750))
@@ -12,6 +12,7 @@ def main():
     # initialize game
     exit = False
     classical_paddle = paddle.Paddle(x_pos=50, y_pos=40)
+    classical_computer = computer.ClassicalComputer(classical_paddle)
     pong_ball = ball.Ball()
     moving_sprites = pygame.sprite.Group()
     moving_sprites.add(classical_paddle)
@@ -24,6 +25,7 @@ def main():
                 exit = True
 
         pong_ball.update()
+        classical_computer.update(pong_ball)
 
         # draw game
         screen.fill(globals.BLACK)
