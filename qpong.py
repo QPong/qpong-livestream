f2b7dbe23427b18336bc78570cd226f1c75b73c4
Junye Huang <h.jun.ye@gmail.com>
2022-12-02 11:43:56 +0100

    step 3: add circuit grid

--- qpong.py
+++ qpong.py
@@ -1,5 +1,7 @@
 import pygame
 
+from assets.circuit_grid import CircuitGrid
+
 pygame.init()
 screen = pygame.display.set_mode((1200, 750))
 pygame.display.set_caption('QPong')
@@ -7,11 +9,22 @@ clock = pygame.time.Clock()
 
 def main():
 
+    # initialize game
+    circuit_grid = CircuitGrid(5, round(750*0.7))
+
     exit = False
     while not exit:
+        # update game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
+            elif event.type == pygame.KEYDOWN:
+                circuit_grid.handle_input(event.key)
+
+        # draw game
+        circuit_grid.draw(screen)
+        pygame.display.flip()
+
         # control framerate
         clock.tick(60)
 
