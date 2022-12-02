f267d320d8d7c78e2db8b9d21c72dc54081cdcb5
Junye Huang <h.jun.ye@gmail.com>
2022-12-02 15:32:02 +0100

    step 4: draw statevector grid

--- qpong.py
+++ qpong.py
@@ -1,7 +1,7 @@
 import pygame
 
 from assets.circuit_grid import CircuitGrid
-from assets import globals
+from assets import globals, ui
 
 pygame.init()
 screen = pygame.display.set_mode((1200, 750))
@@ -23,6 +23,7 @@ def main():
 
         # draw game
         circuit_grid.draw(screen)
+        ui.draw_statevector_grid(screen)
         pygame.display.flip()
 
         # set framerate
