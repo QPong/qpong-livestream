6e199fd1b298c5a6ee1a3ff25e7f896aa8de1fed
Junye Huang <h.jun.ye@gmail.com>
2022-12-02 15:37:51 +0100

    step 5: add classical paddle

--- qpong.py
+++ qpong.py
@@ -1,7 +1,7 @@
 import pygame
 
 from assets.circuit_grid import CircuitGrid
-from assets import globals, ui
+from assets import globals, ui, paddle
 
 pygame.init()
 screen = pygame.display.set_mode((1200, 750))
@@ -11,6 +11,9 @@ clock = pygame.time.Clock()
 def main():
     # initialize game
     circuit_grid = CircuitGrid(5, globals.FIELD_HEIGHT)
+    classical_paddle = paddle.Paddle()
+    moving_sprites = pygame.sprite.Group()
+    moving_sprites.add(classical_paddle)
 
     exit = False
     while not exit:
@@ -24,6 +27,7 @@ def main():
         # draw game
         circuit_grid.draw(screen)
         ui.draw_statevector_grid(screen)
+        moving_sprites.draw(screen)
         pygame.display.flip()
 
         # set framerate
