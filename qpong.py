40f921909e4a0de85f8e09d26101b6168573e3ad
Junye Huang <h.jun.ye@gmail.com>
2022-12-02 11:35:49 +0100

    step 2: create a pygame skeleton

---
+++ qpong.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1200, 750))
+pygame.display.set_caption('QPong')
+clock = pygame.time.Clock()
+
+def main():
+
+    exit = False
+    while not exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit = True
+        # control framerate
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
