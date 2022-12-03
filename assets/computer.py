d7f1a13c9789e104faaa17ae5c75f566421ae14e
Junye Huang <h.jun.ye@gmail.com>
2022-12-03 11:53:02 +0100

    step 9: add quantum computer

--- assets/computer.py
+++ assets/computer.py
@@ -1,3 +1,5 @@
+import qiskit
+
 class Computer:
     def __init__(self):
         pass
@@ -15,3 +17,18 @@ class ClassicalComputer(Computer):
             self.paddle.rect.y -= self.speed
         else:
             self.paddle.rect.y += self.speed
+
+class QuantumComputer(Computer):
+    def __init__(self, quantum_paddles, circuit_grid) -> None:
+        self.paddles = quantum_paddles.paddles 
+        self.score = 0
+        self.circuit_grid = circuit_grid
+
+    def update(self, ball):
+        simulator = qiskit.BasicAer.get_backend("statevector_simulator")
+        circuit = self.circuit_grid.model.compute_circuit()
+        transpiled_circuit = qiskit.transpile(circuit, simulator)
+        statevector = simulator.run(transpiled_circuit, shots=100).result().get_statevector()
+
+        for basis_state, amplitude in enumerate(statevector):
+            self.paddles[basis_state].image.set_alpha(abs(amplitude)**2*255)
