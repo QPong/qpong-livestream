f6defa38d39bc904eff6c84ab1c2de8822cf77d0
Junye Huang <h.jun.ye@gmail.com>
2022-12-03 12:29:42 +0100

    step 11: add collision with quantum paddle after measurement

--- assets/computer.py
+++ assets/computer.py
@@ -1,6 +1,8 @@
 import pygame
 import qiskit
 
+from . import globals
+
 class Computer:
     def __init__(self):
         pass
@@ -27,8 +29,23 @@ class QuantumComputer(Computer):
         self.paddles = quantum_paddles.paddles 
         self.score = 0
         self.circuit_grid = circuit_grid
+        self.measured_state = 0
+        self.last_measurement_time = pygame.time.get_ticks() - globals.MEASUREMENT_COOLDOWN_TIME
 
     def update(self, ball):
+        current_time = pygame.time.get_ticks()
+        # trigger measurement when the ball is close to quantum paddles
+        if 88 < ball.rect.x / globals.WIDTH_UNIT < 92:
+            if current_time - self.last_measurement_time > globals.MEASUREMENT_COOLDOWN_TIME:
+                self.update_after_measurement()
+                self.last_measurement_time = pygame.time.get_ticks()
+        else:
+            self.update_before_measurement()
+    
+        if pygame.sprite.collide_mask(ball, self.paddles[self.measured_state]):
+            ball.bounce() 
+
+    def update_before_measurement(self):
         simulator = qiskit.BasicAer.get_backend("statevector_simulator")
         circuit = self.circuit_grid.model.compute_circuit()
         transpiled_circuit = qiskit.transpile(circuit, simulator)
@@ -36,3 +53,15 @@ class QuantumComputer(Computer):
 
         for basis_state, amplitude in enumerate(statevector):
             self.paddles[basis_state].image.set_alpha(abs(amplitude)**2*255)
+
+    def update_after_measurement(self):
+        simulator = qiskit.BasicAer.get_backend("qasm_simulator")
+        circuit = self.circuit_grid.model.compute_circuit()
+        circuit.measure_all()
+        transpiled_circuit = qiskit.transpile(circuit, simulator)
+        counts = simulator.run(transpiled_circuit, shots=1).result().get_counts()
+        self.measured_state = int(list(counts.keys())[0], 2)
+
+        for paddle in self.paddles:
+            paddle.image.set_alpha(0)
+        self.paddles[self.measured_state].image.set_alpha(255)
